b8bc03c5891eb5cb771374aa7364fcb620c6bd80
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2020-11-19 23:57:10 +0100

    Improve SASS compilation

--- src/css.rs
+++ src/css.rs
@@ -2,8 +2,7 @@ use grass;
 use crate::printing::*;
 
 // Use the grass library to compile Sass to CSS
-pub fn sass_to_css(file_content: String) -> String {
-	let options = grass::Options::default();
+pub fn sass_to_css(file_content: String, options: grass::Options) -> String {
 
 	let css = grass::from_string(file_content, &options);
 
