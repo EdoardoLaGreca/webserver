c16496d3620566d4834ad0fe9f010c4a87780279
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2020-11-20 00:00:25 +0100

    Add error_404() function

--- src/requests_handler.rs
+++ src/requests_handler.rs
@@ -2,9 +2,28 @@ use http::{Request, Response, Method};
 
 use crate::router::get_routes;
 use crate::printing::*;
+use crate::CONFIG;
+use crate::io_ops;
+use crate::defaults;
 
-lazy_static!{
-	static ref ERROR_404: (Vec<u8>, String, u16) = ("ERROR 404: Not found.".into(), "text/plain".into(), 404);
+fn error_404() -> (Vec<u8>, String, u16) {
+
+	if let Ok(content) = io_ops::get_file_content(CONFIG.get_page_404_path(), None) {
+		let mg = mime_guess::from_path(CONFIG.get_page_404_path());
+		let mime_type: String;
+
+		if let Some(t) = mg.first() {
+			mime_type = t.to_string();
+		} else {
+			mime_type = "text/plain".into();
+		}
+
+		(content, mime_type, 404)
+	} else {
+		print_warn(format!("404 error page path \"{}\" does not exist, using default page.", CONFIG.get_page_404_path()));
+
+		(defaults::DEFAULT_404_PAGE_CONTENT.into(), "text/plain".into(), 404)
+	}
 }
 
 // Choose a route based on the method and the URI
@@ -16,19 +35,19 @@ fn choose_route<'a>(req_method: &Method, req_uri: &str) -> Option<(Vec<u8>, Stri
 		}
 	}
 
-	print_err(&format!("Route \"{} {}\" not found.", req_method, req_uri));
+	print_err(format!("Route \"{} {}\" not found.", req_method, req_uri));
 	None
 }
 
 // Returns: body content, content MIME type (html, plain text, etc...), status code
 fn handle_request<'a>(req_method: &Method, req_uri: &str) -> (Vec<u8>, String, u16) {
 
-	print_info(&format!("Request: {} {}", req_method.to_string(), req_uri));
+	print_info(format!("Request: {} {}", req_method.to_string(), req_uri));
 
 	let response: Option<(Vec<u8>, String, u16)> = choose_route(req_method, req_uri);
 
 	if let None = response {
-		return (ERROR_404.0.clone(), ERROR_404.1.clone(), ERROR_404.2);
+		return error_404();
 	}
 
 	return response.unwrap();
