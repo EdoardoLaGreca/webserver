aea552eefb25905e3a8b637dbaee90338e458721
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2020-11-19 23:49:51 +0100

    Update metadata parsing algorithm

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,10 +1,18 @@
 use json;
 
+use std::path::Path;
+
 use crate::io_ops::get_file_content_string;
 use crate::printing::*;
+use crate::defaults;
+
+static mut META_PATH: String = String::new();
 
 #[derive(Clone, Debug, Default)]
 pub struct Config {
+	address: String,
+	default_verbosity: u8,
+	page_404_path: String,
 	pages: Vec<PageMetadata>
 }
 
@@ -24,14 +32,39 @@ pub struct PageTranslation {
 	path: String
 }
 
+pub fn set_meta_path(path: &str) {
+
+	let meta_path: String;
+
+	if !Path::new(path).exists() {
+		print_warn(format!("Specified path \"{}\" does not exists, using default path.", path));
+
+		meta_path = defaults::DEFAULT_META_PATH.into();
+	} else {
+		meta_path = path.into();
+	}
+
+	unsafe {
+		META_PATH = meta_path;
+	}
+}
+
+pub fn get_meta_path() -> String {
+	unsafe {
+		META_PATH.clone()
+	}
+}
+
 impl Config {
-	pub fn parse_metadata(file_path: &str) -> Self {
+
+	// If one or more required values are empty, use default values.
+	pub fn parse_metadata() -> Self {
 		print_info("Reading the JSON metadata file ({})...");
 
-		let metadata_content = get_file_content_string(file_path.into(), None);
+		let metadata_content = get_file_content_string(get_meta_path(), None);
 
 		if let Err(_) = metadata_content {
-			print_warn("Failed reading metadata. Using the default values.");
+			print_warn("Failed reading metadata file. Using the default values.");
 			return Config::default()
 		}
 
@@ -44,14 +77,32 @@ impl Config {
 			return Config::default();
 		}
 
+		// Start parsing
+
 		let metadata = parsed_metadata.unwrap();
 
+		let mut config = Config::default();
+
+		config.address = {
+			if metadata["address"].is_null() {
+				defaults::DEFAULT_ADDRESS.into()
+			} else {
+				metadata["address"].to_string()
+			}
+		};
+
+		config.page_404_path = {
+			if metadata["page_404_path"].is_null() {
+				defaults::DEFAULT_PAGE_404_PATH.into()
+			} else {
+				metadata["page_404_path"].to_string()
+			}
+		};
+
 		if metadata["pages"].is_null() {
-			print_warn(&format!("'pages' array not found in the JSON metadata file. Using the default values."));
+			print_warn(format!("'pages' array not found in the JSON metadata file. Using the default values."));
 			return Config::default();
 		}
-
-		let mut config = Config { pages: vec![] };
 
 		// Get all elements from pages
 		let mut pgs_indx = 0;
@@ -108,6 +159,18 @@ impl Config {
 		}
 
 		return None
+	}
+
+	pub fn get_address(&self) -> String {
+		self.address.clone()
+	}
+
+	pub fn get_default_verbosity(&self) -> u8 {
+		self.default_verbosity
+	}
+
+	pub fn get_page_404_path(&self) -> String {
+		self.page_404_path.clone()
 	}
 }
 
