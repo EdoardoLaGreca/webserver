bda5f94afaf00bef4830a37ffb76fdac0a0eced0
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2021-02-11 01:14:21 +0100

    Now there is an actual method to handle the Markdown to HTML translation. Also improved the code quality and removed useless parts

--- src/html.rs
+++ src/html.rs
@@ -1,19 +1,22 @@
 use comrak::markdown_to_html;
 use regex::Regex;
 
+use crate::io_ops::get_file_content;
 use crate::defaults;
+use crate::printing::print_info;
+use crate::config::CONFIG;
 
 // Convert Markdown into HTML by using comrak
-// markdown_file_content: Content of the markdown file
-// crk_opts:			  comrak options
-// page_title:			  HTML page title (<title> ... </title>)
-// ext_styles:			  External style files (<link rel="stylesheet" href="...">)
-// emb_styles:			  Embedded styles (<style> ... </style>)
-// page_lang:			  Page language (<html lang="...">)
-pub fn build_html_document(markdown_file_content: &str, page_title: &str, ext_styles: Vec<String>, emb_styles: Vec<&str>, page_lang: Option<&str>) -> String {
+// md_fc:				markdown file content
+// crk_opts:			comrak options
+// page_title:			HTML page title (<title> ... </title>)
+// ext_styles:			External style files (<link rel="stylesheet" href="...">)
+// emb_styles:			Embedded styles (<style> ... </style>)
+// page_lang:			Page language (<html lang="...">)
+fn build_html_document(md_fc: &str, page_title: &str, stylesheets: Vec<String>, page_lang: Option<&str>) -> String {
 
 	let mut html_body = markdown_to_html(
-		&markdown_file_content,
+		&md_fc,
 		&defaults::COMRAK_OPTIONS
 	);
 
@@ -27,30 +30,19 @@ pub fn build_html_document(markdown_file_content: &str, page_title: &str, ext_st
 		} else {
 			String::new()
 		}
-		
 	};
+
+	let charset = "<meta charset=\"utf-8\">";
 
 	// Add styles (line by line)
 	let mut html_styles: Vec<String> = vec![];
 
 	// Add <link> styles
-	for link_style in ext_styles {
+	for link_style in stylesheets {
 		html_styles.push(format!(r#"<link rel="stylesheet" href="/style/{}">"#, link_style))
 	}
 
-	// Add embedded styles
-	if emb_styles.len() > 0 {
-		html_styles.push("<style>".into());
-		for embedded_style in emb_styles {
-			html_styles.push(embedded_style.clone().to_owned());
-		}
-		html_styles.push("</style>".into());
-	}
-
-	let html_header: String = format!("<head>\n{}\n{}\n</head>", title, html_styles.join("\n"));
-
-	let html_comment_content = "This document was automatically generated using a custom HTML generator.\nFor more information visit: https://github.com/EdoardoLaGreca/webserver";
-	let html_comment = format!("<!-- {} -->", html_comment_content);
+	let html_header: String = format!("<head>\n{}\n{}\n{}\n</head>", title, charset, html_styles.join("\n"));
 
 	let html_page_lang = {
 		match page_lang {
@@ -59,12 +51,12 @@ pub fn build_html_document(markdown_file_content: &str, page_title: &str, ext_st
 		}
 	};
 
-	let final_html_document = format!("{}\n<!DOCTYPE html>\n<html lang=\"{}\">\n<meta charset=\"utf-8\">\n{}\n{}\n</html>", html_comment, html_page_lang, html_header, html_body);
+	let final_html_document = format!("<!DOCTYPE html>\n<html lang=\"{}\">\n{}\n{}\n</html>", html_page_lang, html_header, html_body);
 
 	final_html_document
 }
 
-pub fn generate_title(path: &str) -> String {
+fn generate_title(path: &str) -> String {
 	// Get the last part of path (filename)
 	let mut page_title = path
 		.split('/')
@@ -82,4 +74,40 @@ pub fn generate_title(path: &str) -> String {
 	}
 
 	page_title
+}
+
+// "Compile" the markdown file into an HTML file.
+// file_path does not include WWW
+pub fn md_to_html(file_path: &String) -> Result<String, ()> {
+
+	let file_content_bytes = get_file_content(&file_path);
+
+	if let Err(_) = file_content_bytes {
+		// File couldn't be read
+		return Err(());
+	}
+
+	// Get file content as string from bytes
+	let file_content_str = String::from_utf8(file_content_bytes.unwrap());
+
+	// The file may be corrupted
+	if let Err(_) = file_content_str {
+		return Err(());
+	}
+
+	print_info(format!("Translating markdown file {}.md into HTML...", file_path));
+
+	let page_title = format!("{} | {}", CONFIG.server.title, generate_title(&file_path));
+
+	// Markdown file translated in HTML
+	let html_translation = build_html_document(
+		&file_content_str.unwrap(),
+		&page_title,
+		vec![defaults::DEFAULT_MD_STYLE.to_owned()],
+		None
+	);
+
+	print_info(format!("Markdown file {}.md translated into HTML", file_path));
+
+	Ok(html_translation)
 }
