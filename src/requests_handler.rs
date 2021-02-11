0354cc2c397175123f78dc4a0f3595864bdf8e1e
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2021-02-11 01:18:13 +0100

    Now the 404 page can be in whatever file format you want, if it's written in Markdown it will be translated into HTML

--- src/requests_handler.rs
+++ src/requests_handler.rs
@@ -2,16 +2,24 @@ use http::{Request, Response, Method};
 
 use crate::router::get_routes;
 use crate::printing::*;
-use crate::config;
+use crate::config::CONFIG;
 use crate::io_ops;
 use crate::defaults;
+use crate::html::md_to_html;
 
 fn error_404() -> (Vec<u8>, String, u16) {
 
-	let config = config::CONFIG.read().unwrap();
+	if let Ok(mut content) = io_ops::get_file_content(&CONFIG.server.err404_path) {
 
-	if let Ok(content) = io_ops::get_file_content(config.get_page_404_path(), None) {
-		let mg = mime_guess::from_path(config.get_page_404_path());
+		// Translate markdown page
+		if CONFIG.server.err404_path.ends_with(".md") {
+			content = md_to_html(&CONFIG.server.err404_path)
+				.unwrap()
+				.as_bytes()
+				.to_vec();
+		}
+
+		let mg = mime_guess::from_path(&CONFIG.server.err404_path);
 		let mime_type: String;
 
 		if let Some(t) = mg.first() {
@@ -22,7 +30,7 @@ fn error_404() -> (Vec<u8>, String, u16) {
 
 		(content, mime_type, 404)
 	} else {
-		print_warn(format!("Path for 404 error page (\"{}\") does not exist, using default page.", config.get_page_404_path()));
+		print_warn(format!("404 error page (\"{}\") does not exist, using default page content.", CONFIG.server.err404_path));
 
 		(defaults::DEFAULT_404_PAGE_CONTENT.into(), "text/plain".into(), 404)
 	}
