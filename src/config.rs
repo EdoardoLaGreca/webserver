b7affab97850398f987619e4f24e4d8f0a3080af
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2021-02-13 02:25:52 +0100

    Improve code

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@ use once_cell::sync::Lazy;
 use serde_derive::Deserialize;
 
 use crate::io_ops;
+use crate::printing::print_err;
 
 pub const CONFIG: Lazy<Config> = Lazy::new(|| Config::new());
 
@@ -10,9 +11,9 @@ pub const CONFIG: Lazy<Config> = Lazy::new(|| Config::new());
 pub struct Server {
 	pub address: String,
 	pub threads: usize,
-	pub err404_path: Path,
+	pub err404_path: String,
 	pub title: String,
-    pub www_path: Path
+    pub www_path: String
 }
 
 #[derive(Deserialize)]
@@ -24,8 +25,15 @@ impl Config {
 	// Returns a new Config instance
 	// Call this function only once in the whole program
 	fn new() -> Config {
-		toml::from_str(get_config_file)
-			.expect(&format!("Couldn't load config.toml: missing/corrupted file or bad syntax."))
+		let config_file_content = io_ops::get_config_file();
+
+		if let Err(_) = config_file_content {
+			print_err("Couldn't open/read config.toml");
+			std::process::exit(1);
+		}
+
+		toml::from_str(&config_file_content.unwrap())
+			.expect(&format!("Couldn't parse config.toml: bad syntax."))
 	}
 }
 
