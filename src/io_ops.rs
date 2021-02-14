71ba69523e6b84885e8692c81e7423f69c9a4b98
EdoardoLaGreca <lagrecaedoardo@gmail.com>
2021-02-14 02:38:42 +0100

    Check if file exists before checking if it's accessible and use the default constant string for config path instead of string literal

--- src/io_ops.rs
+++ src/io_ops.rs
@@ -7,7 +7,9 @@ use crate::printing::*;
 
 // Check if a file can be sent by the webserver by checking if it's inside WWW
 fn is_file_accessible<P: AsRef<Path>>(path: P) -> bool {
-	if path.as_ref().canonicalize().unwrap().starts_with(&config::CONFIG.server.www_path) {
+	let root_dir = Path::new(&config::CONFIG.server.www_path).canonicalize().unwrap();
+
+	if path.as_ref().canonicalize().unwrap().starts_with(&root_dir) {
 		return true;
 	}
 
@@ -17,7 +19,7 @@ fn is_file_accessible<P: AsRef<Path>>(path: P) -> bool {
 // Returns the config file's content (config.toml)
 // This is the only function that can return a file placed outside WWW
 pub fn get_config_file() -> Result<String, ()> {
-	let file_content = fs::read_to_string(Path::new("config.toml"));
+	let file_content = fs::read_to_string(Path::new(defaults::DEFAULT_CONFIG_PATH));
 
 	if let Ok(c) = file_content {
 		return Ok(c);
@@ -30,8 +32,16 @@ pub fn get_config_file() -> Result<String, ()> {
 pub fn get_file_content<P: AsRef<Path>>(path: P) -> Result<Vec<u8>, ()> {
 
 	// Path that includes WWW (but technically still a relative path)
-	let mut complete_path: PathBuf = PathBuf::from(defaults::WWW);
+	let mut complete_path: PathBuf = PathBuf::from(&config::CONFIG.server.www_path);
 	complete_path.push(&path);
+
+	print_info(format!("Getting {} from disk...", complete_path.to_str().unwrap()));
+
+	// Check if the file exists
+	if !path.as_ref().exists() {
+		print_warn(format!("File {} does not exist.", path.as_ref().to_str().unwrap()));
+		return Err(());
+	}
 
 	// Check whether the requested file is contained in WWW
 	if !is_file_accessible(&complete_path) {
@@ -39,16 +49,12 @@ pub fn get_file_content<P: AsRef<Path>>(path: P) -> Result<Vec<u8>, ()> {
 		return Err(());
 	}
 
-	print_info(format!("Getting {} from disk...", complete_path.to_str().unwrap()));
-
 	let content = fs::read(&complete_path);
 
 	if let Err(_) = content {
 		print_err(format!("Error while getting the file {}", complete_path.to_str().unwrap()));
 		return Err(());
 	}
-
-	print_info(format!("Got {} from disk", complete_path.to_str().unwrap()));
 
 	Ok(content.unwrap())
 }
